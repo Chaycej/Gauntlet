04ec9b360036f7dab42998691bb25041c7df1a3f
Chayce Heiberg <theheibergproject@gmail.com>
2018-11-11 16:03:28 -0800

    Reformat code

--- src/gauntlet/Server.java
+++ src/gauntlet/Server.java
@@ -1,17 +1,47 @@
 package gauntlet;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
+/*
+ *  Server
+ * 
+ * The server communicates to clients through a UDP socket on port 3303. The first person
+ * that boots up the game is considered the server, and any player to boot up the game after
+ * the server has been configured is considered a client where they send commands asking the server
+ * to move, attack, etc. 
+ * 
+ * When a client client connects to the server, the server spawns a new thread. The newly created thread's
+ * purpose is only for handling clients, while the main thread handles the server's own game moves/attacks.
+ * 
+ * Server responsibilities include:
+ * 
+ * - Connecting a client to the game.
+ * - Listening for new moves.
+ * - Listening for a client's attack.
+ * - Listening for client's position.
+ * - Sending acknowledgement commands to the client for whether
+ * 	 they are allowed to move or attack.
+ * 
+ */
 public class Server {
 	
+	public static final String YES_CMD = "yes";
+	public static final String NO_CMD  = "no";
 	public static final int PORT = 3303;
 	
 	public DatagramSocket socket;
+	InetAddress clientAddr;
 	
-	// Opens a server UDP socket
+	/*
+	 * Server
+	 * 
+	 * Initializes a server object and opens a UDP socket on port 3303.
+	 */
 	public Server() {
 		try {
 			this.socket = new DatagramSocket(Server.PORT);
@@ -21,7 +51,12 @@ public class Server {
 		System.out.println("DEBUG: Started server");
 	}
 	
-	// Starts the server and begins listening for a client connection
+	/*
+	 * run
+	 * 
+	 * Starts the server and listens for the client connection. Once a client connects
+	 * the server stops listening for new clients.
+	 */
 	public void run() {
 		
 		byte[] buf = new byte[256];
@@ -31,7 +66,7 @@ public class Server {
 			DatagramPacket packet = new DatagramPacket(buf, buf.length);
 			try {
 				this.socket.receive(packet);
-				System.out.println("Received join message from: " + packet.getAddress().toString() + ": " + packet.getPort());
+				System.out.println("Client joined the party: " + packet.getAddress().toString() + ": " + packet.getPort());
 				break;
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -40,6 +75,122 @@ public class Server {
 		}
 	}
 	
+	/*
+	 * sendValidMove
+	 * 
+	 * Acknowledges the client's next move and sends a command allowing the client to move.
+	 */
+	public void sendValidMove(InetAddress clientAddr) {
+		String msg = YES_CMD;
+		byte[] response = msg.getBytes();
+		try {
+			this.socket.send(new DatagramPacket(response, response.length, clientAddr, PORT));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * sendInvalidMove
+	 * 
+	 * Sends an invalid move command to the client.
+	 */
+	public void sendInvalidMove(InetAddress clientAddr) {
+		String msg = NO_CMD;
+		byte[] response = msg.getBytes();
+		try {
+			this.socket.send(new DatagramPacket(response, response.length, clientAddr, PORT));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * readClientMove
+	 * 
+	 * Reads the next move the client is attempting to take and returns it.
+	 * Valid moves are: (up, left, down, right).
+	 * 
+	 */
+	public String readClientMove() {
+		byte[] buf = new byte[256];
+		DatagramPacket pack = new DatagramPacket(buf, buf.length);
+		try {
+			this.socket.receive(pack);
+			this.clientAddr = pack.getAddress();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		String cmd = null;
+		try {
+			cmd = new String(pack.getData(), "UTF-8");
+			cmd = cmd.substring(1,  cmd.charAt(0) - '0'+1);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return cmd;
+	}
+	
+	/*
+	 * readClientPosition
+	 * 
+	 * Reads the client's updated x,y coordinates and returns them as an array.
+	 * 
+	 * [0] -> x coordinate
+	 * [1] -> y coordinate
+	 */
+	public int[] readClientPosition() {
+
+		int[] newPosition = new int[2];
+
+		byte[] buf = new byte[256];
+		DatagramPacket pack = new DatagramPacket(buf, buf.length);
+		try {
+			this.socket.receive(pack);
+			this.clientAddr = pack.getAddress();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Read new client x coordinate
+		String cmd = null;
+		try {
+			cmd = new String(pack.getData(), "UTF-8");
+			cmd = cmd.substring(1,  cmd.charAt(0) - '0'+1);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		newPosition[0] = Integer.valueOf(cmd);
+
+		byte[] buf2 = new byte[256];
+		pack = new DatagramPacket(buf2, buf2.length);
+		try {
+			this.socket.receive(pack);
+			this.clientAddr = pack.getAddress();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Read new client y coordinate
+		cmd = null;
+		try {
+			cmd = new String(pack.getData(), "UTF-8");
+			cmd = cmd.substring(1,  cmd.charAt(0) - '0'+1);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		newPosition[1] = Integer.valueOf(cmd);
+		return newPosition;
+	}
+	
+	/*
+	 * closeServer
+	 * 
+	 * Kills the server and closes the connection between the client.
+	 */
 	public void closeServer() {
 		this.socket.close();
 	}
